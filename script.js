a7e05feb86ee1f4809bbc7169d538dec005b881a
sujallchavan <sujallchavan@gmail.com>
2023-08-19 23:50:17 +0530

    skill section added

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
+        
         body.style.background = 'white';
-        header.style.background='white'
         body.style.color = 'black';
         body.style.transition = '2s';
     }else{
