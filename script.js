6b395f6000f52d526da112b98a13d09af8967b24
sujallchavan <sujallchavan@gmail.com>
2023-08-21 00:29:45 +0530

    oualiication

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ toggle.addEventListener('click', function(){
         body.style.transition = '2s';
     }
 });
+
+
+function switchVisible() {
+    if (document.getElementById('Div1')) {
+
+        if (document.getElementById('Div1').style.display == 'none') {
+            document.getElementById('Div1').style.display = 'block';
+            document.getElementById('Div2').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div1').style.display = 'none';
+            document.getElementById('Div2').style.display = 'block';
+        }
+    }
+}
+
+var typed = new Typed('#element', {
+    strings: ['web devloper.', 'web devlopfffer','wffeb devloper','wffebf devloper','wfxfeb devloper','music','play'],
+    typeSpeed: 50,
+  });
