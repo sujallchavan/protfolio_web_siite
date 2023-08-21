ef4482b6ea4d89cf5c74b1a07c194232efe42493
sujallchavan <sujallchavan@gmail.com>
2023-08-21 22:39:52 +0530

    complit qulification section

--- script.js
+++ script.js
@@ -20,15 +20,31 @@ function switchVisible() {
     if (document.getElementById('Div1')) {
 
         if (document.getElementById('Div1').style.display == 'none') {
-            document.getElementById('Div1').style.display = 'block';
+            document.getElementById('Div1').style.display = 'flex';
             document.getElementById('Div2').style.display = 'none';
         }
         else {
-            document.getElementById('Div1').style.display = 'none';
-            document.getElementById('Div2').style.display = 'block';
+            document.getElementById('Div2').style.display = 'none';
+            document.getElementById('Div1').style.display = 'flex';
         }
     }
 }
+
+
+function switchVisiblee() {
+    if (document.getElementById('Div2')) {
+
+        if (document.getElementById('Div2').style.display == 'none') {
+            document.getElementById('Div2').style.display = 'flex';
+            document.getElementById('Div1').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div1').style.display = 'none';
+            document.getElementById('Div2').style.display = 'flex';
+        }
+    }
+}
+
 
 var typed = new Typed('#element', {
     strings: ['web devloper.', 'web devlopfffer','wffeb devloper','wffebf devloper','wfxfeb devloper','music','play'],
